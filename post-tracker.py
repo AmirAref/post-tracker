be7557c799f1e98023b85a2186e0ebcad3e69ce5
Amir Aref <amiraref808@gmail.com>
2024-07-07 13:15:45 +0330

    add fzf dymenu to cache and load tracking codes

--- post-tracker.py
+++ post-tracker.py
@@ -1,27 +1,82 @@
 #!/bin/env python3
 
 import asyncio
+import os
 from argparse import ArgumentParser
+from pathlib import Path
 
+import pydymenu
 from httpx import AsyncClient
 
+from tracking_post.cli_utils import compat_expanduser
 from tracking_post.errors import TrackingNotFoundError
 from tracking_post.utils import get_tracking_post
+
+XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or compat_expanduser("~/.config")
+CODES_DIR = Path(XDG_CONFIG_HOME, "post-tracker")
+CODES_FILE_PATH = Path(CODES_DIR, "tracking_codes.txt")
 
 parser = ArgumentParser(
     prog="post-tracker",
     description="a command line tool to get tracking information from tracking.post.ir",
 )
 parser.add_argument(
-    "code",
+    "-c",
+    "--code",
     help="tracking code to get data from tracking.post.ir",
+    required=False,
+    default=None,
 )
 
 
+def read_codes():
+    # check file exists
+    if CODES_FILE_PATH.exists():
+        with open(CODES_FILE_PATH) as file:
+            cached_codes = file.read().strip().split("\n")
+            return cached_codes
+    else:
+        raise FileNotFoundError()
+
+
+def save_new_code(code: str):
+    # check file exists
+    with open(CODES_FILE_PATH, mode="a+") as file:
+        file.write("\n" + code)
+
+
 async def main():
+    # check path is exists
+    try:
+        CODES_DIR.mkdir()
+    except FileExistsError:
+        pass
     # get tracking code from args
     args = parser.parse_args()
-    tracking_code: str = args.code
+    tracking_code_arg: str | None = args.code
+
+    # check code is passed or not
+    if tracking_code_arg is None:
+        # load from file
+        # read cached codes and load list of codes
+        try:
+            list_of_codes = read_codes()
+            # check list is not empty
+            if not list_of_codes:
+                raise FileNotFoundError()
+        except FileNotFoundError:
+            # send error message
+            parser.error(
+                "code can not be empry when there is no any cached tracking code !"
+            )
+        choices = pydymenu.fzf(list_of_codes, prompt="Which tracking code ? ")
+        if choices is None:
+            return print("exit the program !")
+        tracking_code: str = choices[0]
+    else:
+        # save to file
+        tracking_code = tracking_code_arg
+        save_new_code(code=tracking_code)
 
     # get data from api
     async with AsyncClient() as client:
---
+++ tracking_post/cli_utils.py
@@ -0,0 +1,21 @@
+import os
+
+compat_os_name = os._name if os.name == "java" else os.name
+
+if compat_os_name in ("nt", "ce"):
+
+    def compat_expanduser(path):
+        home_dir = os.environ.get("HOME")
+        if not home_dir:
+            return os.path.expanduser(path)
+        elif not path.startswith("~"):
+            return path
+        i = path.replace("\\", "/", 1).find("/")  # ~user
+        if i < 0:
+            i = len(path)
+        userhome = (
+            os.path.join(os.path.dirname(home_dir), path[1:i]) if i > 1 else home_dir
+        )
+        return userhome + path[i:]
+else:
+    compat_expanduser = os.path.expanduser
