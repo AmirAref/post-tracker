03b207c9b889d936ecb26fed60bd81f388bfee55
Amir Aref <amiraref808@gmail.com>
2024-09-09 12:04:33 +0330

    feat: add rich tables in cli output

--- post_tracker/main.py
+++ post_tracker/main.py
@@ -7,6 +7,8 @@ from pathlib import Path
 
 import pydymenu
 from httpx import AsyncClient
+from rich.console import Console
+from rich.table import Table
 
 from post_tracker.cli_utils import compat_expanduser
 from post_tracker.errors import TrackingNotFoundError
@@ -39,7 +41,7 @@ def read_codes() -> list[str]:
         raise FileNotFoundError()
 
 
-def save_new_code(code: str):
+def save_new_code(code: str) -> None:
     # check file exists
     try:
         codes = read_codes()
@@ -52,7 +54,7 @@ def save_new_code(code: str):
             file.write("\n".join(codes))
 
 
-async def main():
+async def main() -> None:
     # check path is exists
     try:
         CODES_DIR.mkdir()
@@ -86,13 +88,29 @@ async def main():
         save_new_code(code=tracking_code)
 
     # get data from api
+    table = Table(title="وضعیت مرسوله پستی", show_lines=True, header_style="white bold")
+    table.add_column("وضعیت", justify="right", style="sea_green3")
+    table.add_column("مکان / منطقه", justify="right", style="cyan1")
+    table.add_column("زمان", justify="center", style="spring_green2")
+    table.add_column("تاریخ", justify="right", style="spring_green2")
+    table.add_column("ردیف", justify="center", style="white bold")
     async with AsyncClient() as client:
         try:
             data = await get_tracking_post(client=client, tracking_code=tracking_code)
             # output
-            print(data.model_dump_json(indent=3))
         except TrackingNotFoundError as e:
-            print(e)
+            return print(e)
+
+    for row in data.tracking_list:
+        table.add_row(
+            row.status,
+            row.location,
+            str(row.time),
+            row.date,
+            str(row.index),
+        )
+    console = Console()
+    console.print(table)
 
 
 if __name__ == "__main__":
